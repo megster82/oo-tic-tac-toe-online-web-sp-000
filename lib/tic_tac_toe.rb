3d6e90319cc9181973af282d13029809dd41c289
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 17:04:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   #initialize
   #assigns an instance variable @board to array with 9 blank spaces " "    
   def initialize
-    @board = []
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end 
   #define WIN_COMBINATIONS constant within ttt class = to nested array filled
   #with winning combinations
