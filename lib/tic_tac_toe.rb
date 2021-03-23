93a63e8e343e3e0c8db07ba0b2c92366a05aaf09
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 17:15:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,7 +7,14 @@ class TicTacToe
   #define WIN_COMBINATIONS constant within ttt class = to nested array filled
   #with winning combinations
   WIN_COMBINATIONS = [
-    []
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
     ]
 
   
