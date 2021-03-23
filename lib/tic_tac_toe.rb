8928f61523d2f2214360d54a8aac29869d710671
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 18:36:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,7 +83,7 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |win_combination|
+    WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]      
