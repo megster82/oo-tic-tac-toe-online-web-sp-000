e4196c44be452dcf1d6008843c5012f7c5b7f903
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 16:55:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,82 +1,53 @@
 class TicTacToe
   #initialize
-  #assigns an instance variable @board to array with 9 blank spaces " "
-    def initialize(board = nil)
-      @board = board || Array.new(9," ")
-    end
+  #assigns an instance variable @board to array with 9 blank spaces " "    
+  
+  
   
   #define WIN_COMBINATIONS constant within ttt class = to nested array filled
   #with winning combinations
-  WIN_COMBINATIONS =  [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-    [1, 4, 7],
-    [0, 3, 6],
-    [2, 5, 8]
-    ]
+
+
+
+
   
   def display_board
     #define method that prints current board based on @board instance variable
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+
+
   end
   
   #input_to_index method 
   def input_to_index(user_input)
-    user_input.to_i - 1 
+
+
   end
   
   #move method  
-  def move(index, token = "X")
-    @board[index] = token
+  def move
   end
 
   #position_taken?
-  def position_taken?(index)
-      if @board[index] == " " || @board[index] == "" || @board[index] == nil
-       return false
-     else
-      return true
-      end
+
   end
 
   #valid_move? method that accepts a position to check and returns true
   #if move is valid, false or nil if not
-  def valid_move?(index)
-    if (index < 0 || index > 8)
-      return false
-    elsif position_taken?(index)
-      return false
-    else
-      return true
-    end
+
+
   end
 
   def turn_count
-    @board.count{|token| token == "X" || token == "O"}
+
   end
   
   def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+
   end 
   
   def turn
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-    if valid_move?(index)
-      move(index, current_player)
-      display_board
-    else
-     turn
-    end
-  end
+ 
+   end
 
   def won?
     WIN_COMBINATIONS.each do |win_combination|
