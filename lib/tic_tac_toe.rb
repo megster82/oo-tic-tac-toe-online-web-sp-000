423861eb2140baebb568c7f717cc38319d741e89
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 17:11:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,8 +13,11 @@ class TicTacToe
   
   def display_board(board)
     #define method that prints current board based on @board inst variable
-    puts " #{board[0]} | "
-
+    puts " #{board[0]} |  #{board[1]} |  #{board[2]}"
+    puts "-----------"
+    puts " #{board[3]} |  #{board[4]} |  #{board[5]}"
+    puts "-----------"
+    puts " #{board[6]} |  #{board[7]} |  #{board[8]}"
   end
   
   #input_to_index method 
