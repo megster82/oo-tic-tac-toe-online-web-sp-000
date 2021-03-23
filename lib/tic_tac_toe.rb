20810a92839e8b8d2cc0cce0054a190ec064cd62
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 17:57:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ class TicTacToe
   #valid_move? method that accepts a position to check and returns true
   #if move is valid, false or nil if not
   def valid_move?(index)
-    if (@board[index] == " " || @board[index] == "") && index.between?(1, 9)
+    if position_taken?(index) == false && index.between?(1, 9)
       true
     else
       false 
