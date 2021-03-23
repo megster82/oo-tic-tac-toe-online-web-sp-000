5a3726201c3d1e1565fcbf73e326dbbafadf736c
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 16:56:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,12 +28,12 @@ class TicTacToe
   end
 
   #position_taken?
-
+  def position_taken?
   end
 
   #valid_move? method that accepts a position to check and returns true
   #if move is valid, false or nil if not
-
+  def valid_move?
 
   end
 
@@ -47,7 +47,7 @@ class TicTacToe
   
   def turn
  
-   end
+  end
 
   def won?
 
@@ -57,48 +57,25 @@ class TicTacToe
   
   def full?
 
-
   end
 
   def draw?
-    !(won?) && (full?)
+
   end
   
   def over?
-    if ( won? || draw? || full? )
-      true
-    else
-      false
-    end
+  
+  
   end
 
   def winner
-    if won?
-    WIN_COMBINATIONS.each do |win_combination|
-      position_1 = @board[win_combination[0]]
-      position_2 = @board[win_combination[1]]
-      position_3 = @board[win_combination[2]]
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-        return "X"
-      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return "O"
-      else
-        # do nothing
-      end
-    end
-    else
-    return nil
-    end
+   
+   
   end
 
   def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+  
+  
   end
+
 end 
